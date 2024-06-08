84c0ae8720252d6a7ffe2afc9dee60265ccb5d77
Salauddin Ahmed <110757660+Salauddin2k3@users.noreply.github.com>
2024-06-09 03:28:40 +0600

    first commit

--- index.js
+++ index.js
@@ -27,104 +27,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    const infoCollection = client.db("volunteerStore").collection("volunteerCollection");
-    const NewInfoCollection = client.db("volunteerStore").collection("NewBeVolunteerCollection");
+    const UserInfoCollection = client.db("redAid").collection("userDataCollection");
 
-    app.post("/addPost", async (req, res) => {
-      console.log(req.body);
-      const result = await infoCollection.insertOne(req.body);
-      res.send(result)
-    })
-
-    app.get("/allPost", async (req, res) => {
-      const cursor = infoCollection.find().sort({ deadline: 1 });
-      const allInfo = await cursor.toArray();
-      res.send(allInfo);
-    })
-
-    app.get("/singlePost/:id", async (req, res) => {
-      const result = await infoCollection.findOne({
-        _id: new ObjectId(req.params.id)
-      });
+    // User related API-------------------
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await UserInfoCollection.insertOne(user);
       res.send(result);
     })
 
-    app.put("/updatePost/:id", async (req, res) => {
-      const query = { _id: new ObjectId(req.params.id) };
-      const data = {
-        $set: {
-          postTitle: req.body.postTitle,
-          location: req.body.location,
-          thumbnail: req.body.thumbnail,
-          category: req.body.category,
-          volunteersNeeded: req.body.volunteersNeeded,
-          deadline: req.body.deadline,
-          description: req.body.description
-        }
-      }
-      const result = await infoCollection.updateOne(query, data);
-      res.send(result)
-    })
-
-    // Test $inc Operation
-
-    app.put("/updateINCPost/:id", async (req, res) => {
-      const query = { _id: new ObjectId(req.params.id) };
-      const data = {
-        $inc: {
-          volunteersNeeded: -1
-        }
-      }
-      const result = await infoCollection.updateOne(query, data);
-      res.send(result)
-    })
-
-    app.get("/myInfo/:email", async (req, res) => {
-      console.log(req.params.email);
-      const result = await infoCollection.find({
-        email: req.params.email
-      }).toArray();
-      res.send(result)
-    })
-
-    app.delete("/delete/:id", async (req, res) => {
-      const result = await infoCollection.deleteOne({
-        _id: new ObjectId(req.params.id)
-      })
+    app.get('/users', async (req, res) => {
+      const result = await UserInfoCollection.find().toArray();
       res.send(result);
     })
 
 
-    app.get("/singlePost2/:id", async (req, res) => {
-      const result = await infoCollection.findOne({
-        _id: new ObjectId(req.params.id)
-      });
-      res.send(result);
-    })
 
-    // ------------------------------------------------------------------
-    // New Collection: Be a Volunteer
-
-    app.post("/addNewPost", async (req, res) => {
-      console.log(req.body);
-      const result = await NewInfoCollection.insertOne(req.body);
-      res.send(result)
-    })
-
-    app.get("/myAnotherInfo/:email", async (req, res) => {
-      console.log(req.params.email);
-      const result = await NewInfoCollection.find({
-        email: req.params.email
-      }).toArray();
-      res.send(result)
-    })
-
-    app.delete("/anotherDelete/:id", async (req, res) => {
-      const result = await NewInfoCollection.deleteOne({
-        _id: new ObjectId(req.params.id)
-      })
-      res.send(result);
-    })
 
 
     // Send a ping to confirm a successful connection
